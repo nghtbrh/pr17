d8f5851f7b0f12ba5ec42bd83c91bc563ff58155
Жиляєв Дмитро <dmitrozilaev@gmail.com>
2025-06-07 22:30:07 +0300

    Create GameSettings.java

---
+++ GameSettings.java
@@ -0,0 +1,11 @@
+public class GameSettings {
+    public String player1;
+    public String player2;
+    public int size;
+
+    public GameSettings(String player1, String player2, int size) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.size = size;
+    }
+}
